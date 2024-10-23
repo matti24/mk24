cee56949d382b2ac95431dbbe2f5f9f6c713ab10
matti24 <matti.koenis@swisscom.com>
2024-10-23 10:14:25 +0200

    Update Homepage-SCRIPT.js

--- Homepage-SCRIPT.js
+++ Homepage-SCRIPT.js
@@ -56,10 +56,7 @@
             const hamburgerBtn = document.getElementById('hamburger-btn');
             const menu = document.getElementById('menu');
 
-        hamburgerBtn.addEventListener('click', () => {
-            menu.classList.toggle('open');
-        });
-
+     
 
 
 
