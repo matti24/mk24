9af50c38179da39ed388789e2651594ff53ec21f
matti24 <matti.koenis@swisscom.com>
2024-12-12 11:10:16 +0100

    Update Homepage-SCRIPT.js

--- Homepage-SCRIPT.js
+++ Homepage-SCRIPT.js
@@ -93,9 +93,9 @@
 
     if (counterElement) {
         function updateCounter() {
-            if (count < 2844) {
+            if (count < 3072) {
                 count += 7;  // Zähle um 10 pro Schritt statt um 1
-                counterElement.innerText = count > 2844 ? 2844 : count;  // Sicherstellen, dass er nicht über 4237 hinaus geht
+                counterElement.innerText = count > 3072 ? 3072 : count;  // Sicherstellen, dass er nicht über 4237 hinaus geht
                 setTimeout(updateCounter, 5);
             }
         }
